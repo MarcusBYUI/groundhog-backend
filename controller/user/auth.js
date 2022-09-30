4e93d885f4e4642a5d81a2eb21078a86a0a1f4c9
GopherMines <113481489+GopherMines@users.noreply.github.com>
2022-09-30 18:54:01 +0100

    updated url to demo of front end

--- controller/user/auth.js
+++ controller/user/auth.js
@@ -7,7 +7,7 @@ const createError = require("http-errors");
 const { sendEmail } = require("../../utils/index");
 const Token = require("../../models/user/token");
 
-const url = "http://localhost:3000/verify/";
+const url = "https://main.d2bglchllsa1ed.amplifyapp.com/verify/";
 
 const login = async (req, res, next) => {
   const schema = Joi.object().keys({
